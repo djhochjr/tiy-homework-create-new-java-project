26919cd6261d67882622571e7031bd4947c614ee
dave hochstrasser <davehochstrasser@daves-MacBook-Pro.local>
2016-08-03 21:45:23 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by davehochstrasser on 8/3/16.
+ */
+public class Main {
+    /*declared the main class*/
+    public static void main(String [] args){
+        /*declared the main method*/
+        String dave = "Dave ";
+        String will = "sometimes ";
+        String be = "feels ";
+        String just = "like the ";
+        String great = "New kid on the block ";
+        String thx = "but not ";
+        String doug = "anymore";
+        /*the previous 7 strings were each assigned a different block of text*/
+        String theIronyard = dave +  will +  be + just  + great  + thx  + doug;
+        /*this string was assigned to add the previous strings together*/
+
+        System.out.println(theIronyard);
+        /*tells the summation of theIronyards assignment*/
+    }
+
+}
